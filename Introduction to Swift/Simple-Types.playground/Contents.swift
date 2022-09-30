066270349643e578e60c17b97a8ad904c3dfa35a
Murathaslak <murathaslak@windowslive.com>
2022-09-30 21:15:07 +0300

    variables, simple data types, and string interpolation

---
+++ Introduction to Swift/Simple-Types.playground/Contents.swift
@@ -0,0 +1,40 @@
+import UIKit
+
+var str = "Hello, playground"
+str = "Goodby"
+
+// Strings and integers
+var age = 38
+var population = 8_000_000
+
+// Multi-line strings
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+
+//Doubles and booleans
+var pi = 3.141
+var awasome = true
+
+//String interpolation
+var score = 85
+var string = "Your score was \(score)"
+var result = "The test are here: \(string)"
+
+//Constants
+let taylor = "swift"
+
+//Type annotations
+let str3 = "Hello, playground"
+let album: String = "Reputation"
+let year: Int = 1997
+let height: Double = 1.86
+let taylorRocks: Bool = true
